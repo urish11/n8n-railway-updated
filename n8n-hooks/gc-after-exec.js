57e2d9f86d4dd935d75ce033b4e64e86b021de69
urish11 <117926571+urish11@users.noreply.github.com>
2025-10-05 13:15:04 +0300

    Update gc-after-exec.js

--- n8n-hooks/gc-after-exec.js
+++ n8n-hooks/gc-after-exec.js
@@ -1,12 +1,16 @@
 module.exports = {
-  'workflowExecute.after'(data) {
-    try {
-      if (global.gc) {
-        global.gc();
-        console.log('[n8n hooks] GC executed after workflow.');
-      }
-    } catch (e) {
-      console.error('[n8n hooks] GC failed', e);
-    }
+  workflow: {
+    postExecute: [
+      async function (run, workflowData) {
+        try {
+          if (global.gc) {
+            global.gc();
+            console.log('[n8n hooks] GC executed after workflow.');
+          }
+        } catch (e) {
+          console.error('[n8n hooks] GC failed', e);
+        }
+      },
+    ],
   },
 };
